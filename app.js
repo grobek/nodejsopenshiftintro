18d388eee38b8daebe526039e11a8cc5824def67
work <hayunofek@gmail.com>
2020-06-29 09:02:27 +0300

    fixed v1

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT
+const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP
 
 app.get('/', (req, res) => {
     res.send("Hello from v1!")
 })
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+app.listen(port, ip)
